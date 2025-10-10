da13f8fa9308cb87c48466a12673673e150680b1
LeonardoSantosBR <leosantosmelo07@gmail.com>
2025-10-10 17:50:12 -0300

    fix: 204 para 200

--- src/usecases/users/users-controller.ts
+++ src/usecases/users/users-controller.ts
@@ -67,7 +67,7 @@ export class UsersController {
         });
       }
       await this.userService.patch({ id: Number(id) }, body);
-      return response.status(204).send("Usuário atualizado com sucesso.");
+      return response.status(200).send("Usuário atualizado com sucesso.");
     } catch (error: any) {
       return response.status(500).json({
         message: error.message || "unexpected error",
